bb2f32b3466401932e88abc05495ca832477a0d9
Gevorg Galstyan <g@g.local>
2019-04-12 01:23:24 +0400

    Better printf

--- script.sh
+++ script.sh
@@ -1,5 +1,86 @@
 #!/usr/bin/env bash
 
+# Better printf function
+p_print() {
+	string=$1
+	params=${@:2}
+
+    # Styles and Colors literals
+    STYLE_NONE=00
+    STYLE_BOLD=01
+    STYLE_UNDERSCORE=04
+    STYLE_BLINK=05
+    STYLE_REVERSE=07
+    STYLE_CONCEALED=08
+    COLOR_BLACK=30
+    COLOR_RED=31
+    COLOR_GREEN=32
+    COLOR_YELLOW=33
+    COLOR_BLUE=34
+    COLOR_MAGENTA=35
+    COLOR_CYAN=36
+    COLOR_WHITE=37
+    BG_BLACK=40
+    BG_RED=41
+    BG_GREEN=42
+    BG_YELLOW=43
+    BG_BLUE=44
+    BG_MAGENTA=45
+    BG_CYAN=46
+    BG_WHITE=47
+    NO_STYLE='\e[0m'
+
+	style_string='\e['
+	for param in ${@:2}; do
+		case $param in
+			STYLE_NONE*)		style_string+=$STYLE_NONE";";;
+			STYLE_BOLD*)		style_string+=$STYLE_BOLD";";;
+			STYLE_UNDERSCORE*)	style_string+=$STYLE_UNDERSCORE";";;
+			STYLE_BLINK*)		style_string+=$STYLE_BLINK";";;
+			STYLE_REVERSE*)		style_string+=$STYLE_REVERSE";";;
+			STYLE_CONCEALED*)	style_string+=$STYLE_CONCEALED";";;
+			COLOR_BLACK*)		style_string+=$COLOR_BLACK";";;
+			COLOR_RED*)			style_string+=$COLOR_RED";";;
+			COLOR_GREEN*)		style_string+=$COLOR_GREEN";";;
+			COLOR_YELLOW*)		style_string+=$COLOR_YELLOW";";;
+			COLOR_BLUE*)		style_string+=$COLOR_BLUE";";;
+			COLOR_MAGENTA*)		style_string+=$COLOR_MAGENTA";";;
+			COLOR_CYAN*)		style_string+=$COLOR_CYAN";";;
+			COLOR_WHITE*)		style_string+=$COLOR_WHITE";";;
+			BG_BLACK*)			style_string+=$BG_BLACK";";;
+			BG_RED*)			style_string+=$BG_RED";";;
+			BG_GREEN*)			style_string+=$BG_GREEN";";;
+			BG_YELLOW*)			style_string+=$BG_YELLOW";";;
+			BG_BLUE*)			style_string+=$BG_BLUE";";;
+			BG_MAGENTA*)		style_string+=$BG_MAGENTA";";;
+			BG_CYAN*)			style_string+=$BG_CYAN";";;
+			BG_WHITE*)			style_string+=$BG_WHITE";";;
+			*)					;;
+		esac
+	done
+	[[ style_string == '\e[' ]] && style_string+='0'
+	i=$((${#style_string}-1))
+	[[ ${style_string:$i:1} == ';' ]] && style_string=${style_string:0:$i}
+	style_string+='m'
+	printf "${style_string}${string}${NO_STYLE}"
+}
+
+p_println() { 	p_print "${@}"; echo; }
+p_log() { 		p_print "$1" COLOR_WHITE; }
+p_alert() { 	p_print "$1" COLOR_RED STYLE_BOLD; }
+p_success() { 	p_print "$1" COLOR_GREEN STYLE_BOLD; }
+p_info() { 		p_print "$1" COLOR_CYAN STYLE_BOLD; }
+p_logln() { 	p_log "$1"; echo; }
+p_alertln() { 	p_alert "$1"; echo; }
+p_successln() { p_success "$1"; echo; }
+p_infoln() { 	p_info "$1"; echo; }
+
+prompt() { read -p "$1 " -n 1 -r && echo; [[ $REPLY =~ ^[Yy]$ ]] && true || false; }
+# Usage
+# if prompt "ok? [y/N]"; then
+# 	echo 'OK'
+# fi
+
 # Getting the OS the script is running on
 os_name="$(uname -s)"
 case "${os_name}" in
@@ -27,6 +108,7 @@ self_update() {
     echo "DevEnv Updated!"
 }
 
+# macOS Cleanup
 mac_cleanup() {
     # Empty Trash
 	sudo rm -rfv /Volumes/*/.Trashes &>/dev/null
