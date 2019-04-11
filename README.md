adced226ae605fe1173a4a56042da7a144c573a8
Gevorg Galstyan <g@g.local>
2019-04-12 00:59:37 +0400

    Initial functions

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# devenv
+# DevEnv
+
 Development Environment Setup Script
+
+## Install as `.bash_profile`
+
+To install the script you need to download it and save as `.bash_profile`
+
+```bash
+curl -sL https://raw.githubusercontent.com/93v/devenv/master/script.sh > $HOME/.bash_profile && source $HOME/.bash_profile
+```
+
+## Launch setup
+
+```bash
+setup
+```
+
+## Update
+
+```bash
+selfu
+```
+
+> Note: After some experiments we have realized that we are constantly making
+> the same typo while trying to update and we are typing `slefu` instead.
+> That is why we have both `selfu` and `slefu` as an alias to update.
--- script.sh
+++ script.sh
@@ -1,1 +1,53 @@
 #!/usr/bin/env bash
+
+# Getting the OS the script is running on
+os_name="$(uname -s)"
+case "${os_name}" in
+	Linux*)		machine=Linux;;
+	Darwin*)	machine=Mac;;
+	# CYGWIN*)	machine=Cygwin;;
+	# MINGW32*)	machine=Win32;;
+	# MINGW64*)	machine=Win64;;
+	*)			machine="UNKNOWN:${os_name}"
+esac
+
+if [[ ${machine} == UNKNOWN* ]]; then echo "Unsupported OS ${machine}. Exiting..."; return; fi
+
+# Setup
+setup() {
+    [[ ${machine} == "Mac" ]] && echo "macOS setup scripts coming soon..."
+	[[ ${machine} == "Linux" ]] && echo "Linux setup scripts coming soon..."
+}
+
+# Self Update
+self_update() {
+    UPDATE_URL="https://raw.githubusercontent.com/93v/devenv/master/script.sh"
+    curl -sL $UPDATE_URL > $HOME/.bash_profile
+    source $HOME/.bash_profile
+    echo "DevEnv Updated!"
+}
+
+alias r="source $HOME/.bash_profile"
+alias selfu="self_update"
+alias slefu="self_update"
+
+# ------------------------------------------------------------------------------
+# Launching a function from this file directly
+# ------------------------------------------------------------------------------
+
+# You can launch any function from this file by simply calling
+# bash script.sh function_name
+# bash script.sh function_name arg1 arg2
+
+# Check if the function exists (bash specific)
+if [ -n "$1" ]; then
+    if declare -f "$1" > /dev/null;
+    then
+    	# call arguments verbatim
+    	"$@"
+    else
+    	# Show a helpful error
+    	echo "'$1' is not a known function name" >&2
+    	exit 1
+    fi
+fi
