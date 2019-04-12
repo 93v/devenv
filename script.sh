1c88cff2adb5236f0877f9b2514753fb5f90142e
Gevorg Galstyan <g@g.local>
2019-04-13 01:21:23 +0400

    initializers

--- script.sh
+++ script.sh
@@ -425,6 +425,89 @@ alias selfu="self_update"
 # slefu is a usual typo
 alias slefu="self_update"
 
+if [[ ${machine} == "Mac" ]]; then
+	export PATH=$HOME/bin:/opt/local/bin:/opt/local/sbin:/usr/local/opt/ruby/bin:/usr/local/sbin:$PATH
+	export JAVA_HOME=$(/usr/libexec/java_home)
+
+	[ $(which nodenv) ] && eval "$(nodenv init -)"
+	[ $(which pyenv) ] && eval "$(pyenv init -)"
+	[ $(which rbenv) ] && eval "$(rbenv init -)"
+	[ $(which goenv) ] && eval "$(goenv init -)"
+
+	# If Visual Studio Code is not installed and VS Code Insiders is installed
+	# alias code to code-insiders
+	[ ! $(which code) ] && [ $(which code-insiders) ] && alias code="code-insiders"
+
+	[ -f $(brew --prefix)/etc/bash_completion ] && . $(brew --prefix)/etc/bash_completion
+
+	###-begin-npm-completion-###
+	#
+	# npm command completion script
+	#
+	# Installation: npm completion >> ~/.bashrc  (or ~/.zshrc)
+	# Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
+	#
+
+	if type complete &>/dev/null; then
+		_npm_completion () {
+			local words cword
+			if type _get_comp_words_by_ref &>/dev/null; then
+				_get_comp_words_by_ref -n = -n @ -n : -w words -i cword
+			else
+				cword="$COMP_CWORD"
+				words=("${COMP_WORDS[@]}")
+			fi
+			local si="$IFS"
+			IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
+								COMP_LINE="$COMP_LINE" \
+								COMP_POINT="$COMP_POINT" \
+								npm completion -- "${words[@]}" \
+								2>/dev/null)) || return $?
+			IFS="$si"
+			if type __ltrim_colon_completions &>/dev/null; then
+				__ltrim_colon_completions "${words[cword]}"
+			fi
+		}
+		complete -o default -F _npm_completion npm
+	elif type compdef &>/dev/null; then
+		_npm_completion() {
+			local si=$IFS
+			compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+						COMP_LINE=$BUFFER \
+						COMP_POINT=0 \
+						npm completion -- "${words[@]}" \
+						2>/dev/null)
+			IFS=$si
+		}
+		compdef _npm_completion npm
+	elif type compctl &>/dev/null; then
+		_npm_completion () {
+			local cword line point words si
+			read -Ac words
+			read -cn cword
+			let cword-=1
+			read -l line
+			read -ln point
+			si="$IFS"
+			IFS=$'\n' reply=($(COMP_CWORD="$cword" \
+							COMP_LINE="$line" \
+							COMP_POINT="$point" \
+							npm completion -- "${words[@]}" \
+							2>/dev/null)) || return $?
+			IFS="$si"
+		}
+		compctl -K _npm_completion npm
+	fi
+	###-end-npm-completion-###
+
+	vol() {
+		while true
+		do
+			osascript -e "set volume output volume ${1:-50}"
+		done
+	}
+fi
+
 # ------------------------------------------------------------------------------
 # Launching a function from this file directly
 # ------------------------------------------------------------------------------
